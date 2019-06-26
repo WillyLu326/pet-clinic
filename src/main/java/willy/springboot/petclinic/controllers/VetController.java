48cd89e0a89d6a731b692a48998ae57fe95c350e
Willy Lu <luzh0326@gmail.com>
2019-06-25 21:32:47 -0400

    create VetController and vet thymeleaf

---
+++ src/main/java/willy/springboot/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package willy.springboot.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("vets")
+public class VetController {
+
+    @GetMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
